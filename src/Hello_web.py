856cb9172330ac0ffba1968df03ae8952b1e5ffd
孙锋 <sunqinwang@foxmail.com>
2016-10-18 18:44:31 +0800

    加入静态页面及其映射。

--- src/Hello_web.py
+++ src/Hello_web.py
@@ -1,9 +1,18 @@
+from httplib import error
+
 from flask import Flask
+from flask.templating import render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '<div style="font-size:60px">Hello World!</div>'
+
+@app.route('/foo')
+def foo():
+    return render_template("foo.html",error=error)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
